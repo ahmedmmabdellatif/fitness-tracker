1f08bd898c565bf11e75168a36148c657f895438
ahmedmmabdellatif <ahmed.m.m.abdellatif@gmail.com>
2025-05-16 18:31:32 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlanView from './components/PlanView';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <PlanView />
+    </div>
+  );
+}
+
+export default App;
