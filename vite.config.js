a90e468b3c6fa6a8c2580fd9293ed55277ebc2b6
ahmedmmabdellatif <ahmed.m.m.abdellatif@gmail.com>
2025-05-18 20:41:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/fitness-tracker/', // this is REQUIRED for GitHub Pages
   plugins: [react()],
-  base: './'
-});
+})
